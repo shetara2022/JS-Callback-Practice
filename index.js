94f6662b542d3e309f247b7fd5377c4e389c4c68
Tara Smith <tarasmith@Taras-Air.lan>
2022-06-29 19:18:29 -0400

    refactor code

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+//track the character's position
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+move(character).moveWithArrowKeys(100, 250)
+//move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+//let direction = null;
+let x = 100;
+let y = 250;
+
+ function moveCharacter() {
+//move character west; x should decrease
+if (direction === 'west'){
+    x = x - 1
+}
+//move character north; y should decrease
+if(direction === 'north'){
+    y = y + 1
+}
+//move character east; x should increase
+if(direction === 'east'){
+    x = x + 1 
+}
+//move character south; y should decrease
+if(direction === 'south'){
+    y = y - 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+}
+
+//setInterval calls a function at specified intervals in milliseconds
+setInterval (moveCharacter, 1)
+//use event key (e) function to chec which key is pressed
+document.addEventListener('keydown', function(e){  
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+ document.addEventListener('keyup', function(e){
+     direction = null
+})
+
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
